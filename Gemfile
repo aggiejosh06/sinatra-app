546d9b838cece0128e2c4c5476fb74063ff9fb57
Joshua Roberson <aggiejosh06@gmail.com>
2018-03-15 17:59:17 -0500

    Completed Chapter 14: Web Apps

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '2.4.1'
+
+gem 'sinatra'
+
+group :development do
+  gem 'sinatra-contrib'
+end
---
+++ app.rb
@@ -0,0 +1,24 @@
+require "sinatra"
+require "sinatra/reloader"
+require "movie"
+require "movie_store"
+
+store = MovieStore.new('movies.yaml')
+
+get("/movies") do
+  @movies = store.all
+  erb :index
+end
+
+get("/movies/new") do
+  erb :new
+end
+
+post("/movies/create") do
+  @movie = Movie.new
+  @movie.title = params["title"]
+  @movie.director = params["director"]
+  @movie.year = params["year"]
+  store.save(@movie)
+  redirect "/movies/new"
+end
